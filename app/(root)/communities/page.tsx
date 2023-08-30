86b01e13e02bb73343f3d9c0a96251b4662192ea
Pratyush_Sahoo <pratyushsahoo1726@gmail.com>
2023-08-31 00:20:58 +0530

    next commit

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,3 +1,15 @@
+/*
+
+Create Organisation --> Clicking "create organisation", its immediately going to open up a greate interface where we can simply upload a profile photo and enter the organisation name, a slide URL and we are good to go.
+
+Question -> How is data from clerk going to get into our application instantly ?
+            How we gonna append additional posts once they are made as part of our organisation ?
+
+Answer -> To do all these stuffs, we have Webhooks waiting and listening for the events from clerk so we can then make additional actions and modify our database accordingly.
+
+*/
+
+
 const page = async () => {
     return (
       <section>
